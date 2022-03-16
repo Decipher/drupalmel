29b8df0e74ab9a83941e8658c407f56417558b1e
Stuart Clark <stu@rtclark.net>
2022-03-16 16:41:28 +1100

    chore(#1): update frontend

---
+++ nuxt/components/app/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+  <!-- https://daisyui.com/components/footer/#footer-with-copyright-text-and-social-icons -->
+  <footer class="footer footer-center p-4 bg-base-300 text-base-content">
+    <div>
+      <p>Copyright © 2022 - All right reserved by Drupal Melbourne</p>
+    </div>
+  </footer>
+</template>
